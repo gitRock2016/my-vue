ba7fcdd6ea233275a4b9632b14e44c3e90190a79
Akihiro Iwata <iakihiro22@gmail.com>
2020-08-23 20:44:26 +0900

    作業用プロジェクトの作成

---
+++ js/my-vue.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+	el:'#app',
+  data:{
+	  message:'hello world!!aaasdfaa',
+    count:0,
+    user:{
+    	lastName:'Iwata',
+      firstname:'Akihiro',
+      prefecture:'Ehime'
+    },
+    colors:['red','green','blue']
+  }
+})
