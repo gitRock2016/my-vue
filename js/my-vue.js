2afa6d73ac8c70afb89515b032f1d143b019e366
Akihiro Iwata <iakihiro22@gmail.com>
2020-09-13 12:15:33 +0900

    TODOアプリの作成（0913のUdemy講座の受講）

--- js/my-vue.js
+++ js/my-vue.js
@@ -1,13 +1,37 @@
 var app = new Vue({
-	el:'#app',
-  data:{
-	  message:'hello world!!aaasdfaa',
-    count:0,
-    user:{
-    	lastName:'Iwata',
-      firstname:'Akihiro',
-      prefecture:'Ehime'
+  el: "#app",
+  data: {
+    message: "hello world!!aaasdfaa",
+    count: 0,
+    user: {
+      lastName: "Iwata",
+      firstname: "Akihiro",
+      prefecture: "Ehime"
     },
-    colors:['red','green','blue']
+    colors: ["red", "green", "blue"]
   }
-})
+});
+
+var todo = new Vue({
+  el: "#todo",
+  data: {
+    newItem: "",
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      // alert();
+      // 未入力の場合は追加しない
+      if (this.newItem == "") return;
+
+      var todo = {
+        item: this.newItem,
+        isDone: false
+      };
+
+      this.todos.push(todo);
+      // 入力欄初期化
+      this.newItem = "";
+    }
+  }
+});
